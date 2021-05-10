ca2c7a13a5ec1184cefa2bc2154876ce22a84ff5
Adam K Dean <adamkdean@googlemail.com>
2021-05-10 15:30:30 +0100

    feat: switch from solid to outline icon style

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,9 +3,9 @@
     <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
       XE Wallet
     </h2>
-    
+
     <p class="flex items-center text-sm text-gray-500 text-center pt-2">
-      <StatusOnlineIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" aria-hidden="true" />
+      <LockOpenIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" aria-hidden="true" />
       Heroicon example
     </p>
 
@@ -14,12 +14,13 @@
 </template>
 
 <script>
-import { StatusOnlineIcon } from '@heroicons/vue/solid'
+// import { LockOpenIcon } from '@heroicons/vue/solid'
+import { LockOpenIcon } from '@heroicons/vue/outline'
 
 export default {
   name: 'Index',
   components: {
-    StatusOnlineIcon
+    LockOpenIcon
   },
   title() {
     return 'XE Wallet » Create or restore a wallet'
