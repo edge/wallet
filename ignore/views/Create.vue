310f5c608acd190c0c69cb9874c9564fa24cd603
Adam K Dean <adamkdean@googlemail.com>
2021-05-10 10:43:04 +0100

    chore: add old test components for future reference

---
+++ ignore/views/Create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="create">
+    <h1>Create wallet</h1>
+    <router-link to="/">Back</router-link>
+    <CreateWallet />
+  </div>
+</template>
+
+<script>
+import CreateWallet from '@/components/CreateWallet.vue'
+
+export default {
+  name: 'Create',
+  metaInfo: {
+    title: 'Create wallet'
+  },
+  components: {
+    CreateWallet
+  }
+}
+</script>
