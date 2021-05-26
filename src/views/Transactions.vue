080fda6036364782eb458c48679f370dae8f85ef
James Lambie <jameslambie@gmail.com>
2021-05-26 16:01:21 +0100

    feat: list transactions

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -1,6 +1,7 @@
 <template>
   <Header/>
-  <AccountPanel />
+  <AccountPanel :wallet="this.wallet" />
+
   <div class="bg-gray-200 py-35">
     <div class="container">
       <TransactionsTable :transactions="transactions"/>
@@ -10,119 +11,22 @@
 
 <script>
 
-import Header from "@/components/Header";
-import TableItem from "@/components/TransactionsTableItem";
-import TransactionsTable from "@/components/TransactionsTable";
-import AccountPanel from "@/components/AccountPanel";
+import Header from "@/components/Header"
+import TableItem from "@/components/TransactionsTableItem"
+import TransactionsTable from "@/components/TransactionsTable"
+import AccountPanel from "@/components/AccountPanel"
+
+import { fetchTransactions, fetchWallet } from '../utils/api'
+import { getWalletAddress } from '../utils/wallet'
 
 export default {
   name: 'Transactions',
   data: function () {
     return {
-      transactions: [{
-        type: 'Received',
-        id: 'dy781263f817268553487y434132123123',
-        date: '16/04/2021 13:06',
-        address: 'dy781263f8176796834279887321312',
-        description: 'Lorem ipsum',
-        amount: 387.9270987
-      },
-        {
-          type: 'Sent',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        },
-        {
-          type: 'Received',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        },
-        {
-          type: 'Received',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        },
-        {
-          type: 'Received',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        },
-        {
-          type: 'Sent',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        },
-        {
-          type: 'Received',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        },
-        {
-          type: 'Received',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        },
-        {
-          type: 'Sent',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        },
-        {
-          type: 'Received',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        },
-        {
-          type: 'Received',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        },
-        {
-          type: 'Received',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        },
-        {
-          type: 'Received',
-          id: 'dy781263f817268553487y434132123123',
-          date: '16/04/2021 13:06',
-          address: 'dy781263f8176796834279887321312',
-          description: 'Lorem ipsum',
-          amount: 387.9270987
-        }]
+      loading: false,
+      metadata: {},
+      transactions: [],
+      wallet: {}
     }
   },
   components: {
@@ -130,6 +34,28 @@ export default {
     TransactionsTable,
     TableItem,
     Header
+  },
+  mounted () {
+    this.loadWallet()
+  },
+  methods: {
+    async fetchTransactions () {
+      const { transactions, metadata } = await fetchTransactions(this.wallet.address)
+
+      this.transactions = transactions
+      this.metadata = metadata  
+      this.loading = false
+    },
+    fetchWallet (address) {
+      return fetchWallet(address)
+    },
+    async loadWallet () {
+      this.loading = true
+
+      const walletAddress = await getWalletAddress()
+      this.wallet = await this.fetchWallet(walletAddress)
+      this.fetchTransactions()
+    }
   },
   title() {
     return 'XE Wallet » Transactions'
