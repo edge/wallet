c967fe4430bf54aa32384104fab9f61f6b2307fb
James Lambie <jameslambie@gmail.com>
2021-06-02 17:42:10 +0100

    fix: link to overview

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <strong class="logo block w-52 md:w-38 flex-shrink-0">
-    <router-link to="/">
+    <router-link to="/overview">
       <img src="/assets/logo.svg" alt="XE Wallet">
     </router-link>
   </strong>
