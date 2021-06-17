f517b90e5441b5a2c296fca6faa81fabfefc6771
Adam K Dean <adamkdean@googlemail.com>
2021-06-17 15:29:51 +0100

    feat: add formatting to Amount component

--- src/components/Amount.vue
+++ src/components/Amount.vue
@@ -1,11 +1,16 @@
 <template>
-  <span class="amount">{{ value }}<sub>{{ currency }}</sub></span>
+  <span class="amount">{{ format(value) }}<sub>{{ currency }}</sub></span>
 </template>
 
 <script>
 export default {
-name: "Amount",
-  props: ['value', 'currency']
+  name: 'Amount',
+  props: ['value', 'currency'],
+  methods: {
+    format(input) {
+      return Number(input).toLocaleString('en-US', { maximumFractionDigits: 6 })
+    }
+  }
 }
 </script>
 
