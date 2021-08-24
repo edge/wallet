7ab1b26f04a8af57e2a9796fc9c1884b79777e01
James Lambie <jameslambie@gmail.com>
2021-08-24 17:05:01 +0100

    refactor: rearrange table columns to match explorer

--- src/components/TransactionsTable.vue
+++ src/components/TransactionsTable.vue
@@ -3,12 +3,12 @@
     <table>
       <thead class="hidden lg:table-header-group">
       <tr>
-        <th width="14%">Date</th>
-        <th width="33%">To/From</th>
-        <th width="9%">Tx Hash</th>
-        <th width="18%">Memo</th>
-        <th width="10%">Status</th>
-        <th width="16%">Amount</th>
+        <th width="10%">Tx Hash</th>
+        <th width="12%">Date</th>
+        <th width="33%">From/To</th>
+        <th width="16%">Memo</th>
+        <th width="12%">Status</th>
+        <th width="20%">Amount</th>
       </tr>
       </thead>
       <tbody v-if="transactions.length">
@@ -18,23 +18,21 @@
       </tbody>
       <tbody v-if="!transactions.length">
         <tr>
-          <td colspan="7" class="py-35 bg-white w-full text-center">
+          <td colspan="7" class="block w-full text-center bg-white lg:table-cell py-35">
             No transactions.
           </td>
         </tr>
       </tbody>
     </table>
-    <!-- <Pagination /> -->
   </div>
 </template>
 
 <script>
-import TransactionsTableItem from "@/components/TransactionsTableItem";
-import Pagination from "@/components/Pagination";
+import TransactionsTableItem from "@/components/TransactionsTableItem"
 
 export default {
   name: "TransactionsTable",
-  components: {Pagination, TransactionsTableItem},
+  components: {TransactionsTableItem},
   props: ['transactions'],
   methods: {
   }
