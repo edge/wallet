85b79274519c19d053aeb96b9d244f515ed26afa
Adam K Dean <adamkdean@googlemail.com>
2021-09-07 13:42:21 +0100

    chore: update news promo text

--- src/components/NewsPromo.vue
+++ src/components/NewsPromo.vue
@@ -5,7 +5,7 @@
       <img src="/assets/e-wiki-promo.png">
     </div>
     <div class="bg-white rounded mt-4 px-12 py-10">
-      <p>Learn about the technologies behind the platform, find out how to contribute to the network, and read answers to common support questions and more.</p>
+      <p>The Community Wiki is the go to resource for all things Edge. Learn about the tech, find out how to contribute, and get answers to common support questions.</p>
       <div class="w-full text-right">
         <!-- <a href="https://edge.network/news/article/here" target="_about" class="button button--outline py-8 mr-8">More info</a> -->
         <a href="https://wiki.edge.network" target="_about" class="button button--solid py-8">Edge Wiki</a>
