fdae6cba156ac159e00562f9aa6f7567755999cb
Will <willgarrett64@yahoo.com>
2022-02-02 13:48:12 +0000

    feat: create first draft of new pagination component

---
+++ src/components/PaginationNew.vue
@@ -0,0 +1,118 @@
+<template>
+  <nav class="pagination">
+    <ol class="pagination__list">
+      <li class="pagination__item">
+        <router-link
+          v-if="currentPage !== 1"
+          @click="setFirstPage"
+          :to="{ name: baseRoute, params: { page: 1 }}"
+        >
+          First
+        </router-link>
+        <span class="not-link" v-else>First</span>
+      </li>
+      <li class="pagination__item">
+        <router-link
+          v-if="currentPage !== 1"
+          @click="setPrevPage"
+          :to="{ name: baseRoute, params: { page: currentPage - 1 }}"
+        >
+          <ChevronLeftIcon/>
+        </router-link>
+        <span class="not-link" v-else><ChevronLeftIcon/></span>
+      </li>
+      <li class="pagination__item">
+        <span v-if="totalPages">
+          Page {{Number(currentPage).toLocaleString()}} of {{Number(totalPages).toLocaleString()}}
+        </span>
+        <span v-else>&nbsp;</span>
+      </li>
+      <li class="pagination__item">
+        <router-link
+         v-if="currentPage < totalPages"
+          @click="setNextPage"
+         :to="{ name: baseRoute, params: { page: currentPage + 1 }}"
+        >
+          <ChevronRightIcon/>
+        </router-link>
+        <span class="not-link" v-else><ChevronRightIcon/></span>
+      </li>
+      <li class="pagination__item">
+        <router-link
+          v-if="currentPage < totalPages"
+          @click="setLastPage"
+          :to="{ name: baseRoute, params: { page: totalPages }}"
+        >
+          Last
+        </router-link>
+        <span class="not-link" v-else>Last</span>
+      </li>
+    </ol>
+  </nav>
+</template>
+
+<script>
+import {ChevronLeftIcon, ChevronRightIcon} from "@heroicons/vue/solid";
+
+export default {
+  name: "Pagination",
+  components: {ChevronRightIcon, ChevronLeftIcon},
+  props: ['baseRoute', 'limit', 'skip', 'totalCount', 'updateSkip'],
+  computed: {
+    currentPage() {
+      return Math.floor(this.skip / this.limit) + 1
+    },
+    totalPages() {
+      return Math.ceil(this.totalCount / this.limit)
+    }
+  },
+  methods: {
+    setFirstPage() {
+      this.updateSkip(0)
+    },
+    setLastPage() {
+      this.updateSkip((this.totalPages - 1) * this.limit)
+    },
+    setNextPage() {
+      this.updateSkip(this.skip + this.limit)
+    },
+    setPrevPage() {
+      this.updateSkip(this.skip - this.limit)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.pagination {
+  @apply mt-10;
+}
+
+.pagination__list {
+  @apply flex items-center flex-wrap justify-center md:justify-end text-sm2 space-x-4;
+}
+
+.pagination__item {
+  @apply bg-white rounded text-gray-400;
+}
+
+.pagination__item a, .pagination__item span {
+  @apply py-6 px-12 block;
+}
+
+.pagination__item a {
+  @apply hover:text-green;
+}
+
+.pagination__item span.not-link {
+  @apply opacity-25;
+}
+
+.pagination__item .active {
+  @apply text-green;
+}
+
+.pagination__item svg {
+  @apply w-18;
+}
+</style>
