66212d80ab3fec5186608b3c2ddb1eef34077bd4
Will <willgarrett64@yahoo.com>
2022-02-02 13:48:46 +0000

    feat: add new pagination to staking view

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -6,7 +6,16 @@
     <div class="container">
       <StakesTable
         :limit="pageLimit"
+        :receiveMetadata="onStakesUpdate"
         :skip="skip"
+      />
+      <Pagination
+        v-if="totalCount > pageLimit"
+        baseRoute="Staking"
+        :limit="pageLimit"
+        :updateSkip="onPageUpdate"
+        :skip="skip"
+        :totalCount="totalCount"
       />
     </div>
   </div>
@@ -15,20 +24,33 @@
 <script>
 import AccountPanel from '@/components/AccountPanel'
 import Header from '@/components/Header'
+import Pagination from '@/components/PaginationNew'
 import StakesTable from '@/components/StakesTable'
 
 export default {
   name: 'Staking',
   data: function () {
     return {
-      pageLimit: 20,
+      metadata: null,
+      pageLimit: 2,
       skip: 0,
+      totalCount: 0
     }
   },
   components: {
     AccountPanel,
     Header,
+    Pagination,
     StakesTable
+  },
+  methods: {
+    onStakesUpdate(metadata) {
+      this.totalCount = metadata.totalCount
+    },
+    onPageUpdate(skip) {
+      this.skip = skip
+      console.log(this.skip)
+    }
   }
 }
 </script>
